2931c5a9e92f8db8b6e4615d289f1527a3264f19
FatalEagle <li.yikuan@gmail.com>
2014-05-19 14:25:34 -0400

    Task Miruka
    Task Miruka

---
+++ treestripper.py
@@ -0,0 +1,26 @@
+#from __future__ import with_statement
+import os
+import re
+COUNT_CARRIAGE=True
+SPACE_SENSITIVE=False
+disallowed=[__file__[__file__.rfind('\\')+1:], r".*?.jpg", r".*?.png"]
+for root, dirs, files in os.walk('.'):
+    if root.endswith(os.path.sep+"testdata"):
+        for f in map(lambda x: os.path.join(root, x), files):
+            for bad in disallowed:
+                if re.match(bad, f):
+                    break
+            else:
+                with open(f, "rb") as fi:
+                    data=fi.read()
+                if COUNT_CARRIAGE:
+                    print "%s: %d carriage returns found"%(f, data.count("\r"))
+                with open(f, "wb") as fo:
+                    if SPACE_SENSITIVE:
+                        fo.write(data.replace("\r\n", "\n"))
+                    else:
+                        for line in data.split('\n'):
+                            if line:
+                                fo.write(line.rstrip())
+                                fo.write('\n')
+raw_input("Press enter to continue")
