ecde4526a6bdff6329261a9b5fc2a6efab620a8e
Alex Li <alextrovert@gmail.com>
2014-05-19 14:40:46 -0400

    Disallowed .exe files from being stripped

--- treestripper.py
+++ treestripper.py
@@ -1,20 +1,23 @@
 #from __future__ import with_statement
 import os
 import re
-COUNT_CARRIAGE=True
-SPACE_SENSITIVE=False
-disallowed=[__file__[__file__.rfind('\\')+1:], r".*?.jpg", r".*?.png"]
+
+COUNT_CARRIAGE = True
+SPACE_SENSITIVE = False
+
+disallowed = [__file__[__file__.rfind('\\')+1:], r".*?.jpg", r".*?.png", r".*?.exe"]
+
 for root, dirs, files in os.walk('.'):
-    if root.endswith(os.path.sep+"testdata"):
+    if root.endswith(os.path.sep + "testdata"):
         for f in map(lambda x: os.path.join(root, x), files):
             for bad in disallowed:
                 if re.match(bad, f):
                     break
             else:
                 with open(f, "rb") as fi:
-                    data=fi.read()
+                    data = fi.read()
                 if COUNT_CARRIAGE:
-                    print "%s: %d carriage returns found"%(f, data.count("\r"))
+                    print "%s: %d carriage returns found." % (f, data.count("\r"))
                 with open(f, "wb") as fo:
                     if SPACE_SENSITIVE:
                         fo.write(data.replace("\r\n", "\n"))
@@ -23,4 +26,4 @@ for root, dirs, files in os.walk('.'):
                             if line:
                                 fo.write(line.rstrip())
                                 fo.write('\n')
-raw_input("Press enter to continue")
+raw_input("Press enter to continue...")
