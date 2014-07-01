bc2cab68cdacd27c4bb260b8e6b3e0a411e9bc83
FatalEagle <li.yikuan@gmail.com>
2014-06-30 22:18:48 -0400

    Stripped files and changed A and G

--- A - Distinct Prime Factors/solution.py
+++ A - Distinct Prime Factors/solution.py
@@ -1,5 +1,5 @@
 # Distinct Prime Factors
-# Official Solution - O(N log log N)
+# Official Solution - O(N log N)
 # By Timothy Li
 
 MAXN = 1000001
@@ -8,7 +8,7 @@ for i in xrange(2, MAXN):
   if not sieve[i]:
     for j in xrange(i, MAXN, i):
       sieve[j] += 1
-X = raw_input()
-Y = raw_input()
+X = int(raw_input())
+Y = int(raw_input())
 for i in xrange(X, Y + 1):
   print sieve[i]
