2512fba16e4c350a36a68d5c72616d3b13540e21
Alex Li <alextrovert@gmail.com>
2014-06-30 23:16:48 -0400

    Revision

--- treestripper.py
+++ treestripper.py
@@ -1,12 +1,9 @@
 #from __future__ import with_statement
 import os
 import re
-
 COUNT_CARRIAGE = True
 SPACE_SENSITIVE = False
-
 disallowed = [__file__[__file__.rfind('\\')+1:], r".*?.jpg", r".*?.png", r".*?.exe"]
-
 for root, dirs, files in os.walk('.'):
     if root.endswith(os.path.sep + "testdata"):
         for f in map(lambda x: os.path.join(root, x), files):
