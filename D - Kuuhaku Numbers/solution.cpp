c11afba469aecdb78f6511ef797af88d7681b3c5
Alex Li <alextrovert@gmail.com>
2014-07-06 22:18:28 -0400

    D solution

--- D - Kuuhaku Numbers/solution.cpp
+++ D - Kuuhaku Numbers/solution.cpp
@@ -1,24 +1,28 @@
 // Kuuhaku Numbers
-// Official Solution - O((base^7) * log(B))
+// Official Solution - O((base^6)*digits)
 // By Timothy Li
 
-#include <iostream>
+#include <algorithm>
+#include <cstdio>
 #include <cstring>
 using namespace std;
 
 int num[19], len;
-long long DP[19][2][2][11][11][11][11][11];
+long long DP[19][2][2][2][11][11][11][11][11];
 
-long long solve(int pos, bool small, bool zero,
+long long solve(int pos, bool small, bool zero, bool more,
                 int A, int B, int C, int D, int E) {
-  long long &ret = DP[pos][small][zero][A][B][C][D][E];
+  long long &ret = DP[pos][small][zero][more][A][B][C][D][E];
   if (ret != -1) return ret;
+  ret = 0;
   int limit = small ? num[pos] : 9;
   if (pos == len) {
-    ret = (E > limit) ? 0 : (limit - E + 1);
+    if (more)
+      ret = limit + 1;
+    else if (E <= limit)
+      ret = limit - E + 1;
   } else {
     int nA = A, nB = B, nC = C, nD = D, nE = E;
-    ret = 0;
     for (int i = 0; i <= limit; i++) {
       if (!zero || i > 0) {
         nA = min(A, i);
@@ -27,8 +31,8 @@ long long solve(int pos, bool small, bool zero,
         nD = min(D, i >= C ? i : 10);
         nE = min(E, i >= D ? i : 10);
       }
-      ret += solve(pos + 1, small && i == limit,
-                   zero && i == 0, nA, nB, nC, nD, nE);
+      ret += solve(pos + 1, small && i == limit, zero && i == 0,
+                   more || E <= i, nA, nB, nC, nD, nE);
     }
   }
   return ret;
@@ -41,18 +45,18 @@ void init(long long N) {
 
 long long get(long long sub) {
   long long N;
-  cin >> N;
+  scanf("%lld", &N);
   N -= sub;
   len = 0;
   init(N);
   len--;
   memset(DP, -1, sizeof DP);
-  return solve(0, 1, 1, 10, 10, 10, 10, 10);
+  return solve(0, 1, 1, 0, 10, 10, 10, 10, 10);
 }
 
 int main() {
   long long A = get(1);
   long long B = get(0);
-  cout << B - A << endl;
+  printf("%lld\n", B - A);
   return 0;
 }
